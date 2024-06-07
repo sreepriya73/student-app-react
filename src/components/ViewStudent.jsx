c4a974d868cdfeba8c4db29ecd7f6e3d07b33e47
sreepriya73 <sreepriyaillikkatt@gmail.com>
2024-06-07 22:21:09 +0530

    comptd

--- src/components/ViewStudent.jsx
+++ src/components/ViewStudent.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const ViewStudent = () => {
+    const [data,changedData]=useState(
+    [
+        {"name":"sree","adnNo":101,"rollNo":12},
+        {"name":"sree","adnNo":101,"rollNo":12},
+        {"name":"sree","adnNo":101,"rollNo":12},
+
+    ]
+)
     return (
         <div>
             <NavBar/>
@@ -11,106 +19,30 @@ const ViewStudent = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                           {data.map(
+                            (value,index) => {
+                                return  <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
                                 <div class="card">
                                     <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
                                         <div class="card-body">
-                                            <h5 class="card-title">SREE</h5>
-                                            <p class="card-text">STUDENT</p>
+                                            <h5 class="card-title">{value.name}</h5>
+                                            <p class="card-text">ADMNno:{value.adnNo}</p>
+                                            <p class="card-text">rollNo:{value.rollNo}</p>
                                             <a href="#" class="btn btn-primary">MORE </a>
                                         </div>
                                 </div>
                             </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">PRIYA</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">ARUN</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE </a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">VISHWA</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE </a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">SANDRA</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">STINA</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">NEHA</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">SREELAKSHMI</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">RAHUL</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE </a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">SONA</h5>
-                                            <p class="card-text">STUDENT</p>
-                                            <a href="#" class="btn btn-primary">MORE</a>
-                                        </div>
-                                </div>
-                            </div>
+                            }
+                            
+                           )}
+                            
+                           
+                           
+                            
+                            
+                           
+                            
+                            
                         </div>
                     </div>
                 </div>
